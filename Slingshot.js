0cba9b3db35af661ece2c64bdb62ac96bade2cd9
rushimasally13 <rushima.sally@gmail.com>
2021-06-16 16:53:37 +0530

    Added Catapult

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage('sprites/sling1.png')
+        this.sling2 = loadImage('sprites/sling2.png')
+        this.sling3 = loadImage('sprites/sling3.png')
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,13 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
     
